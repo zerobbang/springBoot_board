da61f5826a59ac3d63ca4f2784a4e552450a420f
zerobbang <gydiane@naver.com>
2022-07-12 14:25:36 +0900

    공통 모듈utils 생성 및 공통 컨트롤러에서 알러트 메세지 출력

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.board.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.constant.Method;
 import com.board.domain.BoardDTO;
 import com.board.service.BoardService;
+import com.board.util.UiUtils;
 
 @Controller
-public class BoardController {
+public class BoardController extends UiUtils{
 
 	@Autowired
 	private BoardService boardService;
@@ -43,23 +47,79 @@ public class BoardController {
 	
 	
 //	글 등록 화면
-//	BoardDTO 유저가 쓴 데이터 값들
-	@PostMapping(value = "/board/register.do")
-	public String registerBoard(final BoardDTO params) {
-		try {
-			boolean isRegistered = boardService.registerBoard(params);
-			if (isRegistered == false) {
-				// TODO => 게시글 등록에 실패하였다는 메시지를 전달
-			}
-		} catch (DataAccessException e) {
-			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+//	BoardDTO 유저가 쓴 데이터 값들	
+//		UiUtils 함수 추가
+		@PostMapping(value = "/board/register.do")
+		public String registerBoard(final BoardDTO params, Model model) {
+			try {
+				boolean isRegistered = boardService.registerBoard(params);
+				if (isRegistered == false) {
+					return showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/board/list.do", Method.GET, null, model);
+				}
+			} catch (DataAccessException e) {
+				return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
 
-		} catch (Exception e) {
-			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+			} catch (Exception e) {
+				return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+			}
+
+			return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/board/list.do", Method.GET, null, model);
 		}
 		
-//		등록이 잘 되면 리스트로 이동
-		return "redirect:/board/list.do";
-	}
+		
+		
+	
+	
+//	목록 리스트 구현
+	@GetMapping(value = "/board/list.do")
+	public String openBoardList(Model model) {
+		List<BoardDTO> boardList = boardService.getBoardList();
+		model.addAttribute("boardList", boardList);
 
+		return "board/list";
+	}
+	
+
+//	
+	@GetMapping(value = "/board/view.do")
+	public String openBoardDetail(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		
+		if (idx == null) {
+			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+
+		BoardDTO board = boardService.getBoardDetail(idx);
+		if (board == null || "Y".equals(board.getDeleteYn())) {
+			// TODO => 없는 게시글이거나, 이미 삭제된 게시글이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+		model.addAttribute("board", board);
+
+		return "board/view";
+	}
+	
+	@PostMapping(value = "/board/delete.do")
+	public String deleteBoard(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			return showMessageWithRedirect("올바르지 않은 접근입니다.", "/board/list.do", Method.GET, null, model);
+		}
+
+		try {
+			boolean isDeleted = boardService.deleteBoard(idx);
+			if (isDeleted == false) {
+				return showMessageWithRedirect("게시글 삭제에 실패하였습니다.", "/board/list.do", Method.GET, null, model);
+			}
+		} catch (DataAccessException e) {
+			return showMessageWithRedirect("데이터베이스 처리 과정에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+
+		} catch (Exception e) {
+			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list.do", Method.GET, null, model);
+		}
+
+		return showMessageWithRedirect("게시글 삭제가 완료되었습니다.", "/board/list.do", Method.GET, null, model);
+	}
+	
 }
+	
+	
